ecd4604220513d901b09560f75220c2934dba133
luiszhou <luiszhou.start@gmail.com>
2017-02-28 23:11:53 +0800

    remove .idea

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Timer from './Timer';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+    <Greeting isLoggedIn={false}/>,
+    document.getElementById('root')
+)
+;
 
-ReactDOM.render(
-    <Timer />,
-    document.getElementById('timer')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+//
+// ReactDOM.render(
+//     <Timer />,
+//     document.getElementById('timer')
+// );
 
 
