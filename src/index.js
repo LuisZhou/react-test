1dc074dc66fa25933b652f380cbc5316eb86d6e8
luiszhou <luiszhou.start@gmail.com>
2017-02-28 22:45:53 +0800

    First commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Timer from './Timer';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+    <Timer />,
+    document.getElementById('timer')
+);
+
+
