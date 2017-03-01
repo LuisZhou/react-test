806e13aa3706da1c91c17f547bd791505772b714
luiszhou <luiszhou.start@gmail.com>
2017-03-01 23:43:29 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Timer from './Timer';
 import './index.css';
-import Greeting from './Conditional-rendering'
+import Greeting, {LoginControl} from './Conditional-rendering'
 
 ReactDOM.render(
-  <Greeting isLoggedIn={true} />,
+  /*<Greeting isLoggedIn={true} />,*/
+    <LoginControl />,
   document.getElementById('root')
 );
 
